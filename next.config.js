eac88a126516aa1f981e9380e3316e8ab2e30687
truongdb <engtruong1609@gmail.com>
2024-07-02 09:39:05 +0700

    ff

--- next.config.js
+++ next.config.js
@@ -13,6 +13,14 @@ const nextConfig = {
 			},
 		],
 	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'http://192.168.0.128:8080/api/v1/:path*',
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
